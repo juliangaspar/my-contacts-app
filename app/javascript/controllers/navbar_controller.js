c6d4f458b46f6d2c2d7b4ce28825d868146416bb
julian gaspar <juliangaspar15@gmail.com>
2023-01-28 17:41:01 +0100

    Added style to home page and forms

---
+++ app/javascript/controllers/navbar_controller.js
@@ -0,0 +1,17 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="navbar"
+export default class extends Controller {
+  static targets = ["navbar"]
+
+  connect() {
+  }
+
+  updateNavbar() {
+    if (window.scrollY >= window.innerHeight) {
+      this.element.classList.add("navbar-model-white")
+    } else {
+      this.element.classList.remove("navbar-model-white")
+    }
+  }
+}
